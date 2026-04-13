b1194d27d267b381b5aaef392a4a3da91e60a2f4
bilalpatel813 <Bilalpatel78600@gmail.com>
2026-04-13 23:27:11 +0530

    add user url

--- pieblogs/blogweb/blog/views.py
+++ pieblogs/blogweb/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import View
 from django.contrib.auth.models import User
 from django.http import HttpResponse
@@ -8,20 +8,33 @@ from django.urls import reverse_lazy
 from .models import Post
 # Create your views here. 
 class HomeView(LoginRequiredMixin,ListView):
-     model=Post
+     model=Post 
      template_name="blog/home.html"
      context_object_name="posts"
      
-class ProfileView(LoginRequiredMixin,ListView):
+     
+class ProfileView(LoginRequiredMixin,UserPassesTestMixin,ListView):
+    login_url='login'
     model= Post
     template_name="blog/profile.html"
     slug_field = 'username'
     slug_url_kwarg = 'username'
     context_object_name="posts"
+    def test_func(self):
+            return self.request.user.is_authenticated
+       
     def get_queryset(self):
         username = self.kwargs.get('username')
+        
         return Post.objects.filter(author__username=username)
-       
+    def get_context_data(self, **kwargs):
+         context = super().get_context_data(**kwargs)
+         context['profile_user']= get_object_or_404(
+        User, username=self.kwargs['username']
+    )
+         print(self.kwargs)
+         return context
+        
 class PostListView(ListView):
     model= Post
     template_name="blog/Post_list.html"
--- pieblogs/blogweb/blogweb/urls.py
+++ pieblogs/blogweb/blogweb/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
    path("pieblogs/",include('accounts.urls')),
    path("pieblogs/",include('blog.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
