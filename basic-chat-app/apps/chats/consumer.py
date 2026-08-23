5d3b26eb4b1ff04a8b52426626798611e56909e8
bilalpatel813 <patelbilal8137@gmail.com>
2026-08-23 17:08:33 +0530

    multi-user chating

--- basic-chat-app/apps/chats/consumer.py
+++ basic-chat-app/apps/chats/consumer.py
@@ -1,18 +1,41 @@
-# chats/consumers.py
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-from channels.generic.websocket import WebsocketConsumer
 
+class ChatConsumer(AsyncWebsocketConsumer):
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}"
 
-    def disconnect(self, close_code):
-        pass
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name,
+        )
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        await self.accept()
 
-        self.send(text_data=json.dumps({"message": message}))
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name,
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data["message"]
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            },
+        )
+
+    async def chat_message(self, event):
+        await self.send(
+            text_data=json.dumps({
+                "message": event["message"],
+            })
+        )
--- basic-chat-app/config/asgi.py
+++ basic-chat-app/config/asgi.py
@@ -8,20 +8,20 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/asgi/
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
+
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from apps.chats.routing import websocket_urlpatterns
 
-#application = get_asgi_application()
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),# Just HTTP for now. (We can add other protocols later.)
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        ),
-        
-    }
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
+})
